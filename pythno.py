62c8f42e380b211ab71f988f1445bea4992a49c6
Adam Obeng <github@binaryeagle.com>
2015-09-26 17:33:14 -0400

    More complicated fixing and eval
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- pythno.py
+++ pythno.py
@@ -1,62 +1,93 @@
 from pyparsing import *
 import re
 
-op = Suppress(Literal('('))
-cp = Suppress(Literal(')'))
 
-integer_literal = Word(nums)
-expr = Forward()
-operand = Or(integer_literal, expr)
-expr << op + Literal('+') + operand + operand + cp
-rule = OneOrMore(expr)
-
-integer_literal = Word(nums)
-binary_operator = Or([Literal('+'), Literal('-'), Literal('/'), Literal('*')])
-binary_expression = binary_operator + integer_literal + integer_literal
-expr = binary_expression
-pexpr = op + expr + cp
-rule = OneOrMore(pexpr)
-
+def Syntax():
+    operator = oneOf('+ - / *')
+    op = Suppress(Literal('('))
+    cp = Suppress(Literal(')'))
+    int_lit = Word(nums)
+    expr = Forward()
+    atom = int_lit | Group(op + expr + cp)
+    expr << Group(operator + atom + atom)
+    ####
+    #  expr = operator + integer_literal + integer_literal
+    rule = op + expr + cp
+    return rule
 
 test_strings = (
     '(+ 1 2)',
     '(- 1 2)',
+    '(- 1 (+ 1 2))',
     '(+ 1 2',
     '+ 1 2)',
     '+ 1 a)',
+    '(+ 1n2 2)',
 )
 
+
 def parse(s):
-    return rule.parseString(s)
+    return Syntax().parseString(s).asList()
+
+
+def add_to_locals(x, y):
+    locals_[x] = y
 
 OPERATORS = {
-        '+' : lambda x, y: int(x)+int(y),
-        '-' : lambda x, y: int(x)-int(y),
-        '/' : lambda x, y: int(x)/int(y),
-        '*' : lambda x, y: int(x)*int(y),
+    '+': lambda x, y: int(x) + int(y),
+    '-': lambda x, y: int(x) - int(y),
+    '/': lambda x, y: int(x) / int(y),
+    '*': lambda x, y: int(x) * int(y),
+    'define': add_to_locals,
 }
+
+
 def peval(s):
     parsed = parse(s)
-    op, one, two = parsed
-    result = OPERATORS[op](one, two)
+    result = eval_(parsed)
     return result
-peval(test_strings[1])
 
-def parse_fix(s):
-    print 'parsing', s
+
+def eval_(s, locals_={}):
+    if isinstance(s, str):
+        result = s
+    elif isinstance(s, list) and len(s) == 1:
+        result = eval_(s[0])
+    else:
+        op, one, two = s
+        result = OPERATORS[op](eval_(one), eval_(two))
+    return result
+
+
+def parse_fix(s, prefix=''):
+    if len(prefix) > 8:
+        raw_input()
+    print prefix + 'parsing', s
     try:
-        parsed = rule.parseString(s)
+        parsed = Syntax().parseString(s)
         return parsed
     except ParseException as e:
-        # https://github.com/greghaskins/pyparsing/blob/ae9863266c3d75664187d82931226c50b7bba5c2/src/pyparsing.py
         expected = re.match('Expected (.*)', e.msg).group(1)
-        expected = expected.strip('"')
-        if expected == 'W:(0123...)':
-            expected = ' 0 '
-        s = s + ' '
-        s = s[:e.loc] + expected + s[e.loc:]
-        print 'Exception is', e, e.loc
-        #print e.loc, e.line, expected
-        return parse_fix(s)
+        cats = '(W|Re):\((.*)\)'
+        if re.match(cats, expected):
+            t, chars = re.match(cats, expected).groups()
+            chars = chars.strip("'")
+            chars = chars.strip("[]")
+            expected = chars[0]
+            if t == 'W':
+                expected = expected + ' '
+        else:
+            expected = expected.strip('"')
+
+        if e.loc == 0  or e.loc == len(s):
+            # Add char
+            s = s[:e.loc] + expected + s[e.loc:]
+        else:
+            # Replace char
+             s = s[:e.loc] + expected + s[min(e.loc+1, len(s)):]
+        # Also try removing char
+        print prefix + 'Exception is', e, e.loc
+        # print e.loc, e.line, expected
+        return parse_fix(s, prefix=prefix + ' ' * 4)
 for s in test_strings:
-    print s, parse_fix(s)
+    print parse_fix(s)
